b8b75b4b69370d40fcf8ca4c437db2ea0c41e414
占金彬 <oncwnuKss215Ng_Gw5KROX055GoQ@git.weixin.qq.com>
2020-01-20 17:36:42 +0800

    添加需要导入的js文件

--- src/main/java/cn/infomany/Main.java
+++ src/main/java/cn/infomany/Main.java
@@ -66,9 +66,11 @@ public class Main {
 
 
         String scriptHtml = "<link rel=\"stylesheet\" type=\"text/css\" href=\"js/layui/dist/css/layui.css\"/>\n" +
-                "\t\t<script src=\"js/layui/dist/layui.js\" type=\"text/javascript\" charset=\"utf-8\"></script>\n" +
-                "\t\t<script src=\"js/index.js\" type=\"text/javascript\" charset=\"utf-8\"></script><link rel=\"stylesheet\" href=\"css/default.css\">\n" +
-                "\t\t<script src=\"js/highlight.min.js\"></script>" +
+                "<script src=\"js/ScrollMagic.min.js\" type=\"text/javascript\"></script>\n" +
+                "<script src=\"js/clipboard.min.js\" type=\"text/javascript\"></script>\n" +
+                "<script src=\"js/layui/dist/layui.js\" type=\"text/javascript\" charset=\"utf-8\"></script>\n" +
+                "<script src=\"js/index.js\" type=\"text/javascript\" charset=\"utf-8\"></script><link rel=\"stylesheet\" href=\"css/default.css\">\n" +
+                "<script src=\"js/highlight.min.js\"></script>" +
                 "<script>hljs.initHighlightingOnLoad();</script>" +
                 "<link rel=\"stylesheet\" href=\"css/index.css\">";
 
