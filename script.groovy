e2d97199e2d3e97e94f984e26a23357c00b13057
zjbshk <592466695@qq.com>
2020-01-31 19:13:07 +0800

    将html节点移至groovy中创建

--- script.groovy
+++ script.groovy
@@ -1,4 +1,9 @@
-def dealHTML(exportStr, doc) {
+import org.jsoup.nodes.Document
+
+def dealHTML(exportStr, Document doc) {
+
+
+    // 导入脚本文件
     String scriptHtml = '''
             <link rel="stylesheet" type="text/css" href="js/layui/dist/css/layui.css"/>
             <script src="js/ScrollMagic.min.js" type="text/javascript"></script>
@@ -9,9 +14,31 @@ def dealHTML(exportStr, doc) {
             <script src="js/highlight.min.js"></script>
             <script>hljs.initHighlightingOnLoad();</script>
             <link rel="stylesheet" href="css/index.css">'''
-
-    titleEle = doc.getElementsByTag("title").first()
+    def titleEle = doc.getElementsByTag("title").first()
     titleEle.after(scriptHtml)
+
+    // 自定义浏览器的icon
+    doc.head().prepend('<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">')
+
+    // 设置跳转到搭建的yapi
+    doc.select(".nav > a").first()
+            .attr("href", "http://yapi.infomany.cn")
+            .attr("target", "_blank")
+
+    // 设置公司的icon
+    doc.select("svg.svg").first().parent().remove()
+    doc.select(".m-header").first().prepend("<img class='icon' src='img/favicon.png'/>")
+
+
+    // 将title改为当前项目名称
+    doc.select("h1.title").first().text(doc.select("h1.curproject-name").first().text())
+
+    // 添加小火箭
+    doc.body().append('<img id="go_top" class="go_top" src="img/go_top.png"/>')
+
+    // 添加备案号
+    doc.getElementById("right")
+            .append('<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=20000996" target="_blank">赣ICP备20000996</a>')
 
     return doc.toString()
 }
